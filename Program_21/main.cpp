95f45f1fdfb1bce3190942e56a6f5d9e96e33d5b
Lucas Luba <luc.luba@gmail.com>
2018-07-16 19:53:24 +0200

    Display number closest to average program - with pointers v1.1

--- Program_21/main.cpp
+++ Program_21/main.cpp
@@ -14,15 +14,15 @@ int main ()
     cout << "How many numbers do you want to type: ";
     cin >> number;
 
-    // Reserve memory for particular amount of cells and set pointer 'pos' to table[0]
+// Reserve memory for particular amount of cells and set pointer 'pos' to table[0]
     float* table = new float[number];
     float* pos = table;
 
     cout << "Type your numbers using enter as separator: " << endl;
 
-    // Input numbers using pointer with mark on last number
-    for (int i=0; i < number; i++){
-
+// Input numbers using pointer with mark on last number
+    for (int i=0; i < number; i++)
+    {
         if (i == number-1)
         {
             cout << "It's your last number: ";
@@ -33,28 +33,29 @@ int main ()
         pos++;
     }
 
-	  average = sum/number;
+	average = sum/number;
     cout << "Average of all numbers is: " << average <<endl;
 
-		// Reserve memory for second table with diffierence between average and input number
+// Reserve memory for second table with diffierence between average and input number
     
     float* table2 = new float[number];
     float* pos2 = table2;
 
     pos = table;
 
-    for (int k=0; k < number; k++){
-    		*pos2 = abs(*pos - average); // Absolute value of difference between average and input number
+    for (int k=0; k < number; k++)
+    {
+        *pos2 = abs(*pos - average); // Absolute value of difference between average and input number
         pos++;
         pos2++;
     }
 
     pos = table;
     pos2 = table2;
-    nearest = *pos;	// First input number is set as nearest to average
-    dif_1 = *pos2;  // First difference set as the lowest
+    nearest = *pos;     // First input number is set as nearest to average
+    dif_1 = *pos2;      // First difference set as the lowest
 
-    // Loop for checking the lowest difference, start from second number
+// Loop for checking the lowest difference, start from second number
 
     for (int j=1; j < number; j++)
     {
@@ -73,7 +74,7 @@ int main ()
 
     cout << "Nearest numbers to average are: " << nearest; 
 
-    // Loop to check repeats and the same differences
+// Loop to check repeats and the same differences
 
     for (int k=0; k < number; k++)
     {
